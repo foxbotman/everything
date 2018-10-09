b10126320752b4b2c7d5dbf2c15ef38f676a72ce
foxbotman <43978453+foxbotman@users.noreply.github.com>
2018-10-09 15:12:11 +0700

    Update Name.php

--- Name.php
+++ Name.php
@@ -1,8 +1,8 @@
 <?php
-    $server = "localhost";
-    $username = "nutticom_data";
-    $password = "12345";
-    $db = "nutticom_data";
+    $server = "us-cdbr-iron-east-01.cleardb.net";
+    $username = "b798786b8aa714";
+    $password = "2e0e0451";
+    $db = "heroku_ce52199dd4f50e1";
     $conn = new mysqli($server, $username, $password, $db);
 function reply_msg($txtback,$replyToken)//สร้างข้อความและตอบกลับ
 {
